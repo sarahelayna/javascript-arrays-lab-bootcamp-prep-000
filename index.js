7fd9b7cc49053f09c23b2c6eed3fd05d1fd82745
Sarah F <sarahelayna@gmail.com>
2018-01-16 06:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = "I don't do much."
 
-function() {
 var kittens = ["Milo", "Otis", "Garfield"];
-return kittens;
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+  return kittens;
 }
